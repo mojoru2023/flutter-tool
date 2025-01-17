4900f99b28ba9e1c3a23e71eaeb462dd3571d837
MOJORU <34601389+mojoru2023@users.noreply.github.com>
2025-01-17 11:53:33 +0900

    Create d.sh

---
+++ 3.install_supabase(docker)/d.sh
@@ -0,0 +1,17 @@
+# 文档参考： https://supabase.com/docs/guides/self-hosting/docker
+
+
+# Get the code
+git clone --depth 1 https://github.com/supabase/supabase
+
+# Go to the docker folder
+cd supabase/docker
+
+# Copy the fake env vars
+cp .env.example .env
+
+# Pull the latest images
+docker compose pull
+
+# Start the services (in detached mode)
+docker compose up -d
